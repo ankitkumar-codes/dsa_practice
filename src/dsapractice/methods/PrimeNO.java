1abb26491217c455439c70e4b7e0109c66d3a312
Ankit Kumar <ankitk.codes@gmail.com>
2025-09-07 02:29:35 +0530

    checking no is prime or not

---
+++ src/dsapractice/methods/PrimeNO.java
@@ -0,0 +1,22 @@
+package dsapractice.methods;
+
+public class PrimeNO {
+    protected static boolean isPrime(int n) {
+        for (int i = 2; i < Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        if (isPrime(n)) {
+            System.out.println("Given no is prime ");
+        }else {
+            System.out.println("Given no is not prime ");
+        }
+    }
+}
