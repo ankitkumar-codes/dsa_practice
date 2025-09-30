caff981c044f403dc597f2fa918212e80cbf6a1b
Ankit Kumar <ankitk.codes@gmail.com>
2025-09-30 22:03:22 +0530

    Pairs in an array

--- src/dsapractice/strings/StringPallindrome.java
+++ src/dsapractice/strings/StringPallindrome.java
@@ -1,4 +1,35 @@
 package dsapractice.strings;
 
 public class StringPallindrome {
+//        private static boolean isPallindrome(String str) {
+//        String reverseStr = "";
+//
+//        for (int i = str.length()-1; i >= 0; i--) {
+//            reverseStr += String.valueOf(str.charAt(i));
+//
+//        }
+//
+//        if (str.equals(reverseStr)) {
+//            return true;
+//        }else {
+//            return false;
+//        }
+//    }
+
+    private static  boolean isPallindrome(String str) {
+        int n = str.length();
+        for (int i = 0; i < n/2; i++) {
+            if (str.charAt(i) != str.charAt(n-(i+1))) {
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String name = "NAMAN";
+        System.out.println(name.subSequence(1,5));
+        System.out.println("Is " + name + " pallindrome?:- " + isPallindrome(name));
+    }
 }
