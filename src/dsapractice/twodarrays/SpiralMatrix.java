2908cf35532badab5511378bcb1946577f3e7cc2
Ankit Kumar <ankitk.codes@gmail.com>
2025-10-20 00:12:07 +0530

    print spiral matrix

---
+++ src/dsapractice/twodarrays/SpiralMatrix.java
@@ -0,0 +1,54 @@
+package dsapractice.twodarrays;
+
+public class SpiralMatrix {
+    private static void printSpiral(int matrix[][]) {
+        int startRow = 0;
+        int startCol = 0;
+        int endRow = matrix.length-1;
+        int endCol = matrix[0].length-1;
+
+        while (startRow <= endRow && startCol <= endCol) {
+            // top
+            for (int i = startCol; i <= endCol; i++) {
+                System.out.print(matrix[startRow][i] + " ");
+
+            }
+
+            // right
+            for (int i = startRow + 1; i <= endRow; i++) {
+                System.out.print(matrix[i][endCol] + " ");
+
+            }
+
+            // bottom
+            for (int i = endCol-1; i >= startCol; i--) {
+                if (startRow == endRow ) {
+                    break;
+                }
+                System.out.print(matrix[endRow][i] + " ");
+
+            }
+
+            // left
+            for (int i = endRow-1; i > startRow; i--) {
+                if (startCol == endCol) {
+                    break;
+                }
+                System.out.print(matrix[i][startCol] + " ");
+
+            }
+
+            startRow++;
+            startCol++;
+            endRow--;
+            endCol--;
+
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int matrix[][] = {{1,2,3}, {4,5,6}, {7,8,9}};
+        printSpiral(matrix);
+    }
+}
