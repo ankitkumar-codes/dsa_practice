f94e3341d0aa3fb68658ee098b63f999acf96bf8
Ankit Kumar <ankitk.codes@gmail.com>
2025-11-02 22:11:19 +0530

    power calculation

---
+++ src/dsapractice/recursion/PowerFunc.java
@@ -0,0 +1,16 @@
+package dsapractice.recursion;
+
+import java.util.Arrays;
+
+public class PowerFunc {
+    private static int power(int x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        return x * power(x, n-1);
+    }
+    public static void main(String[] args) {
+        System.out.println(power(2,4));
+    }
+}
