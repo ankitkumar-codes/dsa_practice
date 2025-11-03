27db80df3bb97e4095ea96c3a258c3a7edd35659
Ankit Kumar <ankitk.codes@gmail.com>
2025-11-03 21:47:00 +0530

    Optimised power function

--- src/dsapractice/recursion/PowerFunc.java
+++ src/dsapractice/recursion/PowerFunc.java
@@ -10,7 +10,24 @@ public class PowerFunc {
 
         return x * power(x, n-1);
     }
+
+    private static int optimisePower(int x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        int halfPow = optimisePower(x, n/2);
+        int halfPowSq = halfPow * halfPow;
+
+        if (n%2 != 0) {
+            halfPowSq = x * halfPowSq;
+        }
+
+        return halfPowSq;
+    }
+
     public static void main(String[] args) {
         System.out.println(power(2,4));
+        System.out.println("Result through optimised power function: " + optimisePower(2,6));
     }
 }
