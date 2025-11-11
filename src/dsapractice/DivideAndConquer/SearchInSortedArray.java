255e306a0b6273949dbb9f9fca169272271d03b9
Ankit Kumar <ankitk.codes@gmail.com>
2025-11-11 23:23:48 +0530

    Search in sorted array using loop

--- src/dsapractice/DivideAndConquer/SearchInSortedArray.java
+++ src/dsapractice/DivideAndConquer/SearchInSortedArray.java
@@ -30,8 +30,34 @@ public class SearchInSortedArray {
         }
     }
 
+    private static int searchArr(int arr[], int tar, int si, int ei) {
+        while (si <= ei) {
+            int mid = si + (ei - si)/2;
+            if (arr[mid] == tar) {
+                return mid;
+            }
+
+            if (arr[si] < arr[mid]) {
+                if (tar < arr[mid] && tar >= arr[si]) {
+                    ei = mid -1;
+                }else {
+                    si = mid + 1;
+                }
+            }else {
+                if (tar <= arr[ei] && tar > arr[mid]) {
+                    si = mid + 1;
+                }else {
+                    ei = mid -1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
     public static void main(String[] args) {
         int arr[] = {4,5,6,7,0,1,2};
         System.out.println(search(arr, 1, 0, arr.length-1));
+        System.out.println(searchArr(arr, 4, 0, arr.length - 1));
     }
 }
