8fcdd2fadeecb90aef4b4ddcd86d1d29af4629da
Ankit Kumar <ankitk.codes@gmail.com>
2025-11-19 23:55:36 +0530

    Finding even or odd

---
+++ src/dsapractice/bitmanipulation/EvenOdd.java
@@ -0,0 +1,18 @@
+package dsapractice.bitmanipulation;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        if ((n & 1) == 0) {
+            System.out.println(n + " is Even");
+        } else {
+            System.out.println(n + " is Odd");
+        }
+    }
+}
