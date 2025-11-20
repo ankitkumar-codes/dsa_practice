78bc94d542bdf622d735cfdf4314c71a33fc5269
Ankit Kumar <ankitk.codes@gmail.com>
2025-11-20 23:55:30 +0530

    Bit Operations

---
+++ src/dsapractice/bitmanipulation/Operations.java
@@ -0,0 +1,25 @@
+package dsapractice.bitmanipulation;
+
+import java.util.Scanner;
+
+public class Operations {
+
+    // Function to get ith bit
+    public static int getIthBit(int n, int i) {
+        return (n >> i) & 1;   // Right shift then AND with 1
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        System.out.print("Enter bit position (i): ");
+        int i = sc.nextInt();
+
+        int bit = getIthBit(n, i);
+
+        System.out.println("The " + i + "th bit of " + n + " is: " + bit);
+    }
+}
